8bd9e439f49f84376f512b8d6773a12815604a64
Isale <isale.bryan@gmail.com>
2021-08-29 16:03:40 +0300

    fix comment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-// display add book page
 var express = require('express');
 var router = express.Router();
 var dbConn = require('../lib/db');
 
-/* GET users listing. */
+// display add book page
 router.get('/', function(req, res, next) {
   // respond with add.ejs
   res.render('index', {
